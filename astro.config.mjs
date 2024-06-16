a9a3382a48bd38e8ff375e35007bf3003a0266ef
hknsh <valterliviaamaral@gmail.com>
2024-06-16 19:07:54 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
-import cloudflare from "@astrojs/cloudflare";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), svelte()]
 });
---
+++ src/components/Home.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Github from "../svg/tech/github.svelte";
+	import Technologies from "./Technologies.svelte";
+	import I18n from "./i18n.svelte";
+</script>
+
+<div class="md:flex-center flex-col">
+    <div id="#" class="flex flex-col items-start justify-center p-4 font-semibold">
+		<div class="space-y-8">
+			<h1 class="text-3xl md:text-5xl">Hello!</h1>
+			
+			<p>I'm a self-taught JavaScript developer focused in back-end development and a Linux enthusiastic.</p>
+
+			<I18n />
+
+			<p>I started studying programming back in 2017 when I was 11 years old! Since then I've been creating a few projects to improve my skills.</p>
+
+			<div>
+				<Technologies />
+			</div>
+			
+			<div class="flex-center">
+				<div class="flex-center bg-neutral-800 p-3 text-sm md:text-base md:p-4 rounded-xl max-w-sm">
+					<button class="flex-center">
+						<div class="pr-2">
+							<Github />
+						</div>
+						Check this portfolio on GitHub
+					</button>
+				</div>
+			</div>
+		</div>
+    </div>
+</div>
